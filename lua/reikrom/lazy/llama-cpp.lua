92ca2c00d89abbde9deee0919a9f5c746ec37165
Rei Kromulis <reinis.kromulis@ivendi.com>
2025-01-27 12:48:05 +0200

    update local llama

--- lua/reikrom/lazy/llama-cpp.lua
+++ lua/reikrom/lazy/llama-cpp.lua
@@ -2,13 +2,8 @@ return {
   'ggml-org/llama.vim',
   init = function()
     vim.g.llama_config = {
-      --  auto_fim = false,
+      auto_fim = false, -- Ctrl+F + server has to be running
       show_info = false
     }
-
-    vim.keymap.set("n", "<leader>le", function()
-      vim.g.llama_config.auto_fim = true -- Modify the field directly
-      print("Llama Enabled")
-    end)
   end,
 }
