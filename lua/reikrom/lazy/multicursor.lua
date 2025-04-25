aeec096c43d7dd7fe13a3940cb7c7093dd91eca3
Reinis Starks <reinis.kromulis@ivendi.com>
2025-04-25 20:20:42 +0000

    misc: disable multi cursor x bind

--- lua/reikrom/lazy/multicursor.lua
+++ lua/reikrom/lazy/multicursor.lua
@@ -44,7 +44,7 @@ return {
         set({"n", "v"}, "<right>", mc.prevCursor)
 
         -- Delete the main cursor.
-        set({"n", "v"}, "<leader>x", mc.deleteCursor)
+        -- set({"n", "v"}, "<leader>x", mc.deleteCursor)
 
         -- Add and remove cursors with control + left click.
         set("n", "<c-leftmouse>", mc.handleMouse)
