80c467be5da934de587c27298cd4ec69696b6a6e
Reinis Starks <reinis.kromulis@ivendi.com>
2025-08-13 14:20:37 +0300

    update bindings, fugitive, oil

--- lua/reikrom/lazy/fugitive.lua
+++ lua/reikrom/lazy/fugitive.lua
@@ -3,11 +3,11 @@ return {
     config = function()
         vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
 
-        local ThePrimeagen_Fugitive = vim.api.nvim_create_augroup("ThePrimeagen_Fugitive", {})
+        local fugitive_starkbit = vim.api.nvim_create_augroup("fugitive_starkbit", {})
 
         local autocmd = vim.api.nvim_create_autocmd
         autocmd("BufWinEnter", {
-            group = ThePrimeagen_Fugitive,
+            group = fugitive_starkbit,
             pattern = "*",
             callback = function()
                 if vim.bo.ft ~= "fugitive" then
