0d1fdc68c99e641e5894ba705ae692079a8c1433
Reinis Starks <reinis.kromulis@ivendi.com>
2026-04-16 14:55:30 +0300

    swap harpoon binds, g-1, h-2

--- lua/reikrom/lazy/harpoon.lua
+++ lua/reikrom/lazy/harpoon.lua
@@ -8,13 +8,13 @@ return {
     vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
     vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-    vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
-    vim.keymap.set("n", "<C-g>", function() harpoon:list():select(2) end)
+    vim.keymap.set("n", "<C-g>", function() harpoon:list():select(1) end)
+    vim.keymap.set("n", "<C-h>", function() harpoon:list():select(2) end)
     vim.keymap.set("n", "<C-t>", function() harpoon:list():select(3) end)
     vim.keymap.set("n", "<C-y>", function() harpoon:list():select(4) end)
-    vim.keymap.set("n", "<leader><C-h>", function() harpoon:list():replace_at(1) end)
-    vim.keymap.set("n", "<leader><C-g>", function() harpoon:list():replace_at(2) end)
-    vim.keymap.set("n", "<leader><C-t>", function() harpoon:list():replace_at(3) end)
+    vim.keymap.set("n", "<leader><C-g>", function() harpoon:list():replace_at(1) end)
+    vim.keymap.set("n", "<leader><C-h>", function() harpoon:list():replace_at(2) end)
+    --            vim.keymap.set("n", "<leader><C-t>", function() harpoon:list():replace_at(3) end)
     --            vim.keymap.set("n", "<leader><C-n>", function() harpoon:list():replace_at(4) end)
   end
 }
