07812673b701a6035c0b77e7622f01df8cf06b11
Reinis Starks <reinis.starks@github.com>
2026-08-07 18:35:42 +0300

    feat: add new bind, list available treesitter langs

--- lua/reikrom/lazy/treesitter.lua
+++ lua/reikrom/lazy/treesitter.lua
@@ -1,9 +1,15 @@
 return {
     "nvim-treesitter/nvim-treesitter",
+    branch = "main",
     build = ":TSUpdate",
     config = function()
         require("nvim-treesitter").setup()
-
         vim.treesitter.language.register("templ", "templ")
+
+        require("nvim-treesitter").install({ "svelte", "templ", "lua", "vim", "vimdoc", "javascript" })
+
+        vim.keymap.set("n", "<leader>ta", function()
+            vim.print(vim.tbl_keys(require("nvim-treesitter.parsers")._available_parsers))
+        end, { desc = "list available parsers" })
     end
 }
